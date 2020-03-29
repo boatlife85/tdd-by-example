fddb15b0b004f5b32ea58957f32141cee2997bca
Joshua S <35569800+boatlife85@users.noreply.github.com>
2020-03-29 04:22:04 -0400

    Pulled times method from child classes to parent class.  Changed Money class to a concrete class instead of an abstract class, and continued refactoring efforts with passing tests before commit.

--- src/main/java/guru/springframework/Money.java
+++ src/main/java/guru/springframework/Money.java
@@ -1,6 +1,6 @@
 package guru.springframework;
 
-public abstract class Money {
+public class Money {
     protected int amount;
     protected String currency;
 
@@ -13,8 +13,6 @@ public abstract class Money {
         return this.currency;
     };
 
-    public abstract Money times(int multiplier);
-
     public static Money dollar(int amount){return new Dollar(amount, "USD");}
 
     public static Money franc (int amount){return new Franc(amount, "CHF");}
@@ -22,6 +20,18 @@ public abstract class Money {
     public boolean equals(Object object){
         Money money = (Money) object;
         return amount == money.amount
-                && this.getClass().equals((object.getClass()));
+                && this.currency == money.currency;
+    }
+
+    @Override
+    public String toString() {
+        return "Money{" +
+                "amount=" + amount +
+                ", currency='" + currency + '\'' +
+                '}';
+    }
+
+    public Money times(int multiplier) {
+        return new Money(amount * multiplier, this.currency);
     }
 }
